7df80068ba964b2a320c090e12f57db85d93de22
Emma Dolan <edolan5@stanford.edu>
2018-12-03 13:11:06 -0800

    Revert "Revert "Merge branch 'master' of https://github.com/skoire/MotiVote""
    This reverts commit 12cd816cd5631648cff22a0534e0639ed07b937b.

--- screens/BallotScreen.js
+++ screens/BallotScreen.js
@@ -7,142 +7,88 @@ import {
   Text,
   TouchableOpacity,
   View,
+  FlatList,
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
+import { ListItem } from 'react-native-elements'
 
-//TODO: back button on camera 
 
 export default class BallotScreen extends React.Component {
   static navigationOptions = {
-    title: 'voting',
+    header: null,
+  };
+
+  onPress(screen) {
+    this.props.navigation.navigate(screen)
   };
 
   render() {
     return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={require('../assets/images/MotiVote-logo.png')}
-              style={styles.welcomeImage}
-            />
-          </View>
+      <View style={styles.container} behavior="padding" enabled>
+      <Text style = {styles.Header}> Your Ballot </Text>
+      <Text style = {styles.SubHeader}> Learn about what's on your ballot </Text> 
+      <Text style = {styles.Title}> California General Election </Text>
+      <Text style = {styles.Title}> Tuesday, November 6, 2018 </Text>
+      <Text style = {styles.Title}> </Text>
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
+        <FlatList 
+          data = {[{key: 'Propositions', value: '3'}, 
+            {key: 'Governor', value: '2'},
+            {key: 'Secretary of State', value: '2'}, 
+            {key: 'Your Decisions', value: '7'}]}
+          renderItem = {({item}) => (
+          <TouchableOpacity onPress={this.onPress.bind(this, item.key)}>
+          <ListItem 
+          title = {item.key}
+          titleStyle = {styles.TitleFont}
+          badge={{ value: item.value, textStyle: { color: 'white' }, containerStyle: { backgroundColor: '#66257D' } }}
+          chevron = {true} 
+          bottomDivider = {true}
+          />
+          </TouchableOpacity>
+          )}
+        />
+        </View>
 
-            <Text style={styles.getStartedText}>
-              Hello fellow students of Minh-An.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-      </View>
     );
   }
 
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
+  
 }
 
 const styles = StyleSheet.create({
+    Header: {
+      paddingTop: 35,
+      fontSize: 35,
+      color: '#66257D',
+      lineHeight: 40,
+      textAlign: 'left',
+      marginLeft: 13,
+      fontFamily: 'Charter-Bold',
+  },
+     SubHeader:{
+      textAlign: 'left',
+      marginLeft: 20,
+      marginBottom: 30,
+      fontSize: 17,
+      lineHeight: 17,
+      fontFamily: 'Charter',
+  },
+    TitleFont:{
+      fontFamily: 'Charter',
+      marginLeft: 11,
+  },
+    Title:{
+      fontFamily: 'Charter-Bold',
+      fontSize: 20,
+      color: '#66257D',
+      textAlign: 'center'
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
     paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 200,
-    height: 160,
-    resizeMode: 'contain',
-    marginTop: -30,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+    backgroundColor: '#ffffff',
+    flex: 1
   },
 });
