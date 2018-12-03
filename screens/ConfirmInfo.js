7771aaa251d03668e425b33303df5242e3d96edd
Emma Dolan <edolan5@stanford.edu>
2018-12-03 13:07:36 -0800

    disregard this update

--- screens/ConfirmInfo.js
+++ screens/ConfirmInfo.js
@@ -123,9 +123,7 @@ export default class App extends Component {
   handleSubmit = () => {
     const value = this._form.getValue();
     console.log('value: ', value);
-    if(value){
-    	this.props.cb();
-    }
+    this.props.cb();
   }
   // handleSubmit2 = () => {
   // 	this.handleSubmit();
